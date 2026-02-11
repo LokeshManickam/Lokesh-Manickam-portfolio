db8409acda2c10e5196d6b20f851297881f5c4e2
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-02-11 23:20:32 +0530

    Reset repository with new project code

--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { submitContactForm } = require("../controllers/contactController");
-
-// POST /api/contact
-router.post("/", submitContactForm);
-
-module.exports = router;
