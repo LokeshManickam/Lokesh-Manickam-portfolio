cc52799e979456f6066f6312a2b3d1de35230cd9
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-02-12 00:16:07 +0530

    add backend data to frontend

--- frontend/js/contactform.js
+++ frontend/js/contactform.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         submitBtn.innerText = "Sending...";
 
         try {
-            const response = await fetch("http://localhost:5000/api/contact", {
+            const response = await fetch("https://portfolio-backend-iaxw.onrender.com/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
