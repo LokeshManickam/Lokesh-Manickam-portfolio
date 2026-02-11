6344c4b7a7da1a15c1f9a9040f6a53f912bd43a5
LokeshManickam <LOKESHMANICKAM2005@GMAIL.COM>
2026-02-11 23:13:59 +0530

    add

---
+++ backend/models/Contact.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// ✅ THIS LINE IS CRITICAL
+module.exports = mongoose.model("Contact", contactSchema);
